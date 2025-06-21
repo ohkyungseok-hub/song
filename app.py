b09ae9b73efa0b33e4f3996051bfd07be0bc4679
okstm <okstm@lastorder.co.kr>
2025-06-22 03:58:24 +0900

    배포 준비 완료

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def recommend():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(port=5002, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
